09bb6a8c20fa0ba617d6391172f4e5b91d677cd8
patelsachin344 <sachinpatel71344@gmail.com>
2023-02-28 16:05:18 +0530

    server done

--- Server/Db/connect.js
+++ Server/Db/connect.js
@@ -4,17 +4,14 @@ const dotenv = require("dotenv").config();
 
 const connect = () => {
   return new Promise((res, rej) => {
-    mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.jdy4bcc.mongodb.net/nordstromeData?retryWrites=true&w=majority`,
-      (err) => {
-        if (err) {
-          console.error("Error connecting to nordstromeData: " + err);
-          rej();
-        }
-        console.log("Connected to nordstromeData");
-        res();
+    mongoose.connect(`${process.env.DATABASE_URL}`, (err) => {
+      if (err) {
+        console.error("Error connecting to nordstromeData: " + err);
+        rej();
       }
-    );
+      console.log("Connected to nordstromeData");
+      res();
+    });
   });
 };
 
